00af91e454d2e2b2bfa592b7ee62edc98ae7cae9
vitaly.krasnov <vitaly.krasnov@pyramidanalytics.com>
2025-05-22 13:28:19 +0300

    push

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
+  base: 'fishy-fugitive',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
