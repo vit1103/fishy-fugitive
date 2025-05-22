a2ae465914f1dc0304796565a4c6cad989d328e1
vitaly.krasnov <vitaly.krasnov@pyramidanalytics.com>
2025-05-22 14:51:15 +0300

    add chanes

--- vite.config.ts
+++ vite.config.ts
@@ -14,7 +14,7 @@ export default defineConfig(({ mode }) => ({
     mode === 'development' &&
     componentTagger(),
   ].filter(Boolean),
-  base: 'fishy-fugitive',
+  base: './',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
